7a4ff9ea5c12b860e8d79b1bd234b25347b77d00
Shreyas0505 <shreyasguggilla12@gmail.com>
2025-10-18 21:06:05 +0530

    Update model path input to relative path

--- wellness_app.py
+++ wellness_app.py
@@ -43,7 +43,7 @@ st.markdown("Real-time emotion detection with data visualization and storage")
 
 # --- SIDEBAR SETTINGS ---
 st.sidebar.header("⚙️ Settings")
-model_path = st.sidebar.text_input("Model Path", "/home/shreyas/Downloads/model(3).h5")
+model_path = st.sidebar.text_input("Model Path", "model(3).h5")
 cascade_path = st.sidebar.text_input("Cascade Classifier Path", "haarcascade_frontalface_default.xml")
 
 # Session history storage
